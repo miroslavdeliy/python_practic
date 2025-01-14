949ead4577d8aea60cc4fb329878d80564b971e9
Deliy Miroslav <miroslavdely@gmail.com>
2025-01-14 14:12:19 +0300

    1-st version

--- Library/main.py
+++ Library/main.py
@@ -3,6 +3,27 @@ def book_list_view(library):
         print(f"{book}: {library[book]["availability"]}")
 
 
+def add_book(title, author, year):
+    if title in library:
+        check_y_n = input("\nДанная книга уже есть. Обновить? Y/N")
+        if check_y_n.upper() == "Y":
+            library[title] = {
+                "author": author,
+                "year": year,
+                "availability": None
+            }
+            print("\nКнига обновилась.")
+        else:
+            print("\nКнига не обновлена!")
+    else:
+        library[title] = {
+            "author": author,
+            "year": year,
+            "availability": None
+        }
+        print("\nКнига успешно добавилась!")
+
+
 library = {
     "Война и мир": {
         "author": "Лев Толстой",
@@ -23,3 +44,7 @@ library = {
 
 print("Список книг в библиотеке:")
 book_list_view(library)
+add_book("Война и мир", "Александр Дюма", 1890)
+print("\nОбновленный список книг в библиотеке:")
+book_list_view(library)
+
