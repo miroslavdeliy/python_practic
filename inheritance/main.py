51ee9aa94650db3b999f84ab5bc8d7a7c7a9e546
Deliy Miroslav <miroslavdely@gmail.com>
2025-01-21 10:16:47 +0300

    2-st version

--- inheritance/main.py
+++ inheritance/main.py
@@ -1,24 +1,36 @@
 import math
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
+#Абстрактный класс
+class Share(ABC):
 
-class Share:
     @abstractmethod
     def area(self):
         pass
 
+
+#Прямоугольник
 class Rectangle(Share):
-    def area (self, heigh, width):
-        return heigh * width
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def area (self):
+        return self.height * self.width
+
+
+#Круг
 class Circle(Share):
-    def area(self, radius):
-        return math.pi * math.pow(radius, 2)
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
 
 
-share_rectangle = Rectangle()
-share_circle = Circle()
-print(f"Площадь прямоугольника {share_rectangle.area(34, 65)}")
-print(f"Площадь круга {share_circle.area(34)}")
+#Основное тело программы
+share_rectangle = Rectangle(10, 50)
+share_circle = Circle(10)
 
-
+print(f"Площадь прямоугольника {share_rectangle.area()}")
+print(f"Площадь круга {share_circle.area()}")
