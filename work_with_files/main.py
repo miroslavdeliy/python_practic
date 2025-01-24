410dee4b3a325a3733275059abed57c73fdef3a3
Deliy Miroslav <miroslavdely@gmail.com>
2025-01-24 15:50:29 +0300

    2-st version

--- work_with_files/main.py
+++ work_with_files/main.py
@@ -12,17 +12,15 @@ def cheсk_correct(func):
 #Возвращение содержимого файла
 @cheсk_correct
 def open_for_read(name):
-    file = open(name, 'r')
-    line = file.read()
-    file.close()
-    print(line)
+    with open(name, 'r') as file:
+        line = file.read()
+        print(line)
 
 
 #Добавление строки в конец файла
 def add_string(name,text):
-    file = open(name, 'a')
-    file.write(f"\n{text}")
-    file.close()
+    with open(name, 'a') as file:
+        file.write(f"\n{text}")
 
 
 #Чтение построчно
@@ -35,14 +33,14 @@ def process_line(name):
 
 #Копирование бинарного файла
 @cheсk_correct
-def copy_binar_file(name, copy_name):
-    with open(name,'rb') as binar_file:
-        binar_data = binar_file.read()
-    with open(copy_name, 'wb') as binar_file:
-        binar_file.write(binar_data)
+def copy_binary_file(name, copy_name):
+    with open(name,'rb') as binary_file:
+        binary_data = binary_file.read()
+    with open(copy_name, 'wb') as binary_file:
+        binary_file.write(binary_data)
 
 
 open_for_read('data.txt')
 add_string('data.txt', 'Add new line')
 process_line('data.txt')
-copy_binar_file('data.txt','copy_data.txt')
+copy_binary_file('data.txt','copy_data.txt')
